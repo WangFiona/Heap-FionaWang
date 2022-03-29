7a0c4d3e9e1e1224d8c9d264993500aaebc9afc4
WangFiona <fionaw5th@gmail.com>
2022-03-29 13:43:40 -0700

    add works

--- Main.cpp
+++ Main.cpp
@@ -8,11 +8,11 @@ void generate(int* &heap);
 void userInput(int* & heap);
 void add(int* &heap, int data, int index); //Recursive
 void printTree(int* heap);
-void printOutput(int* &heap, int data, int index); //Recursive
+void printOutput(int* &heap, int data, int index, int size); //Recursive
 
 int main(){
   int* heap = new int[100];
-  char* command = new char[15];
+  char command[15];
 
   cout << "Would you like to generate numbers or input your own?" <<
     " (Enter 'generate' or 'input')" << endl;
@@ -26,12 +26,12 @@ int main(){
   for(int i=0; i < strlen(command); i++) {
     command[i] = toupper(command[i]);
   }
-  
+
   if(strcmp(command, "GENERATE") == false){
     generate(heap);
   }
   else if(strcmp(command, "INPUT") == false){
-    //add(heap);
+    userInput(heap);
   }
   else {
     cout << "Not a valid command" << endl;
@@ -41,7 +41,84 @@ int main(){
 }
 
 void generate(int* &heap){
+  char* fileName = new char[20];
+  cout << "Enter the name of the file: (include the '.txt')" << endl;
+  cin >> fileName;
+  fstream file;
+  file.open(fileName);
+
+  int input = 0;
+  int inputNum = 1;
+  while(file >> input){
+    cout << input << " ";
+    add(heap, input, inputNum);
+    inputNum++;
+  }
+  file.close();
+  cout << endl;
+  printTree(heap);
+  printOutput(heap, heap[1], 1, inputNum);
 }
 
 void userInput(int* &heap){
+  bool entering = true;
+  char input[10];
+  cout << "Enter a number or enter \"done\" to print the tree and output"
+       << endl;
+  int inputNum = 1;
+  while(entering){
+    cin >> input;
+    //Allow both upper and lower case
+    for(int i=0; i < strlen(input); i++) {
+      input[i] = toupper(input[i]);
+    }
+
+    bool isNum = true;
+    for(int i=0; i<strlen(input); i++){
+      if(!isdigit(input[i])){
+	isNum = false;
+      }
+    }
+
+    if(strcmp(input, "DONE") == false){
+      printTree(heap);
+      printOutput(heap, heap[1], 1, inputNum);
+      entering = false;
+    }
+    else if(!isNum){
+      cout << "Please enter a number or \"done\"" << endl;
+    }
+    else{
+      int num = atoi(input);
+      add(heap, num, inputNum);
+      cout << "Enter a number or enter \"done\" to print the tree and output"
+	   << endl;
+      inputNum++;
+    }
+  }
+}
+
+void add(int* &heap, int data, int index){
+  if(index > 1){
+    heap[index] = data;
+    int parentIndex = index/2;
+    int parentData = heap[parentIndex];
+    if(data > parentData){
+      heap[parentIndex] = data;
+      heap[index] = parentData;
+      add(heap, data, parentIndex);
+    }
+  } else{
+    heap[index] = data;
+  }
+}
+
+void printTree(int* heap){
+}
+
+void printOutput(int* &heap, int data, int index, int size){
+  for(int i=1; i<size; i++){
+    cout << heap[i] << " ";
+  }
+  cout << endl;
 }
